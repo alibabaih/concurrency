039d49f18198980ca62616b7c4bee1efd3f89239
alibabaih <alibabaih@gmail.com>
2015-01-15 17:21:31 +0400

    add locks

--- src/bank/Account.java
+++ src/bank/Account.java
@@ -1,9 +1,22 @@
 package bank;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Account {
 
     //что быдет если добавить volatile int balance?
     private int balance;
+
+    public Lock getLock() {
+        return l;
+    }
+
+    public void setLock(Lock l) {
+        this.l = l;
+    }
+
+    Lock l = new ReentrantLock();
 
     public int getBalance() {
         return balance;
@@ -13,8 +26,9 @@ public class Account {
         this.balance = balance;
     }
 
-    public Account(int initialBalance) {
+    public Account(int initialBalance, Lock l) {
         this.balance = initialBalance;
+        this.l = l;
     }
 
     public void withdraw(int amount) {
--- src/bank/Operations.java
+++ src/bank/Operations.java
@@ -1,12 +1,23 @@
 package bank;
 
 import javax.naming.InsufficientResourcesException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Operations {
+
+    public static final long WAIT_SEC = 100;
 
     public static void main(String[] args) throws InsufficientResourcesException {
         final Account a = new Account(1000);
         final Account b = new Account(2000);
+
+        //пытаемся избавится от deadlock с помощью Lock and ReentrantLock
+        Lock lock1 = new ReentrantLock();
+        Lock lock2 = new ReentrantLock();
+        lock1.lock();
+        lock2.lock();
 
         new Thread(new Runnable() {
             @Override
@@ -25,15 +36,37 @@ public class Operations {
         System.out.println(b.getBalance());
     }
 
-    static void transfer(Account acc1, Account acc2, int amount) throws InsufficientResourcesException {
-        if (acc1.getBalance() < amount) throw  new InsufficientResourcesException();
+    static void transfer(Account acc1, Account acc2, int amount) throws InsufficientResourcesException, InterruptedException {
+        if (acc1.getLock().tryLock(WAIT_SEC, TimeUnit.SECONDS)) {
+            try {
+                if (acc2.getLock().tryLock(WAIT_SEC, TimeUnit.SECONDS)) {
+                    try {
+                        //Do transfer
+                        acc1.withdraw(amount);
+                        acc2.deposit(amount);
+                    } finally {
+
+                    }
+                }
+            } finally {
+                acc1.getLock().unlock();
+            }
+        } else {
+            //error waiting lock
+
+        }
+
+
+        //if (acc1.getBalance() < amount) throw  new InsufficientResourcesException();
+
         /*
         acc1.withdraw(amount);
         acc2.deposit(amount);
         */
         //пытаемся засинхронизировать доступ к русурсам,
         // но притакой реализации остаётся возможен deadlock (один ждёт другого)
-        synchronized (acc1) {
+
+        //synchronized (acc1) {
             /* имитируем работу deadlock'а
             Thread dump
             in bin directory java jdk execute cmd input jps
@@ -45,10 +78,10 @@ public class Operations {
                 System.out.println("Catch interrupted exception: " + e);
             }
             */
-            synchronized (acc2) {
-                acc1.withdraw(amount);
-                acc2.deposit(amount);
-            }
-        }
+//            synchronized (acc2) {
+//                acc1.withdraw(amount);
+//                acc2.deposit(amount);
+//            }
+        //}
     }
 }
